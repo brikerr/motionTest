7a2e3e7c9665edd9eb4e879f89eb16b6deaade84
Brian Kerr <bkerr79@gmail.com>
2021-02-19 11:51:19 -0800

    updates2

--- motion-test/src/App.js
+++ motion-test/src/App.js
@@ -26,37 +26,52 @@ function getTitle(title) {
 }
 
 const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'http://reactjs.org/',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      tite: 'Redux',
+      url: 'http://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
 
   const handleChange = event => {
-    console.log(event);
+    console.log(event.target.value);
   };
 
   return (
     <div>
       <h1>Hello {getTitle('React')}</h1>
       <label htmlFor="search">Search:</label>
-      <input id="search" type="text" />
+      <input id="search" type="text" onChange={handleChange} />
       <hr />
-      <List />
+      <List list={stories} />
     </div>
   );
 }
 
 
-function List() {
-  return list.map(function (item) {
-    return (
-      <div key={item.objectID}>
-        <span>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span> {item.author} </span>
-        <span> {item.num_comments} </span>
-        <span> {item.points} </span>
-      </div>
-    );
-  });
-}
+const List = props =>
+  props.list.map(item => (
+    <div key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span> {item.author} </span>
+      <span> {item.num_comments} </span>
+      <span> {item.points} </span>
+    </div>
+  ));
+
 
 export default App;
 
