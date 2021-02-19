c6c6115094009bce00733945ef75478727326f3c
Brian Kerr <bkerr79@gmail.com>
2021-02-19 11:11:14 -0800

    updates

--- motion-test/src/App.js
+++ motion-test/src/App.js
@@ -25,7 +25,12 @@ function getTitle(title) {
   return title;
 }
 
-function App() {
+const App = () => {
+
+  const handleChange = event => {
+    console.log(event);
+  };
+
   return (
     <div>
       <h1>Hello {getTitle('React')}</h1>
@@ -56,19 +61,3 @@ function List() {
 export default App;
 
 
-// function List() {
-//   return list.map(function (item) {
-//     return (
-//       <div key={item.objectID}>
-//         <span>
-//           <a href={item.url}>{item.title}</a>
-//         </span>
-//         <span> {item.author} </span>
-//         <span> {item.num_comments} </span>
-//         <span> {item.points} </span>
-//       </div>
-//     );
-//   });
-// }
-
-// export default App;
