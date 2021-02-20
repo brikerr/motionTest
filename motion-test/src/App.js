0504ed86cab382147b8bcc769916468c4d969055
Brian Kerr <bkerr79@gmail.com>
2021-02-19 17:17:54 -0800

    updates3

--- motion-test/src/App.js
+++ motion-test/src/App.js
@@ -1,42 +1,19 @@
+import React from 'react';
 import './App.css';
-
-
-const list = [
-  {
-    title: 'React',
-    url: 'http://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'http://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-
-function getTitle(title) {
-  return title;
-}
 
 const App = () => {
   const stories = [
     {
       title: 'React',
-      url: 'http://reactjs.org/',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
       num_comments: 3,
       points: 4,
       objectID: 0,
     },
     {
-      tite: 'Redux',
-      url: 'http://redux.js.org/',
+      title: 'Redux',
+      url: 'https://redux.js.org/',
       author: 'Dan Abramov, Andrew Clark',
       num_comments: 2,
       points: 5,
@@ -44,21 +21,35 @@ const App = () => {
     },
   ];
 
-  const handleChange = event => {
-    console.log(event.target.value);
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  const handleSearch = event => {
+    setSearchTerm(event.target.value);
   };
+
+  const searchedStories = stories.filter(story =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div>
-      <h1>Hello {getTitle('React')}</h1>
-      <label htmlFor="search">Search:</label>
-      <input id="search" type="text" onChange={handleChange} />
+      <h1>My Hacker Stories</h1>
+
+      <Search onSearch={handleSearch} />
+
       <hr />
-      <List list={stories} />
+
+      <List list={searchedStories} />
     </div>
   );
-}
+};
 
+const Search = props => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" onChange={props.onSearch} />
+  </div>
+);
 
 const List = props =>
   props.list.map(item => (
@@ -66,13 +57,10 @@ const List = props =>
       <span>
         <a href={item.url}>{item.title}</a>
       </span>
-      <span> {item.author} </span>
-      <span> {item.num_comments} </span>
-      <span> {item.points} </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
     </div>
   ));
 
-
 export default App;
-
-
