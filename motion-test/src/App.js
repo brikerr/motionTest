f17cc3497946322948da743a0b19870753be99f8
Brian Kerr <bkerr79@gmail.com>
2021-02-24 22:29:10 -0800

    updates2

--- motion-test/src/App.js
+++ motion-test/src/App.js
@@ -64,18 +64,3 @@ const List = props =>
   ));
 
 export default App;
-
-
-const List = props =>
-  props.list.map(item => (
-    <div key={item.objectID}>
-      <span>
-        <a href={item.url}>{item.title}</a>
-      </span>
-      <span>{item.author}</span>
-      <span>{item.num_comments}</span>
-      <span>{item.points}</span>
-    </div>
-  ));
-
-export default App;
